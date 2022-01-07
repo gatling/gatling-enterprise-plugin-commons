0fc4e5da9a1d906d8655af5a860aff6dfad91a63
tpetillot <th.petillot@gmail.com>
2022-01-07 15:43:36 +0100

    chore: log package upload in non interactive
    Modifications:
    * Require logger in non interactive enterprise plugin
    * Define upload with checksum and log in abstract plugin client class

--- src/main/java/io/gatling/plugin/PluginClient.java
+++ src/main/java/io/gatling/plugin/PluginClient.java
@@ -16,13 +16,33 @@
 
 package io.gatling.plugin;
 
+import static io.gatling.plugin.util.ObjectsUtil.nonNullParam;
+
 import io.gatling.plugin.client.EnterpriseClient;
+import io.gatling.plugin.exceptions.EnterprisePluginException;
+import io.gatling.plugin.io.PluginLogger;
+import java.io.File;
+import java.util.UUID;
 
 abstract class PluginClient {
 
   protected final EnterpriseClient enterpriseClient;
+  protected final PluginLogger logger;
 
-  public PluginClient(EnterpriseClient enterpriseClient) {
+  public PluginClient(EnterpriseClient enterpriseClient, PluginLogger logger) {
     this.enterpriseClient = enterpriseClient;
+    this.logger = logger;
+  }
+
+  protected long uploadPackageWithChecksum(UUID packageId, File file)
+      throws EnterprisePluginException {
+    nonNullParam(packageId, "packageId");
+    nonNullParam(file, "file");
+    if (enterpriseClient.uploadPackageWithChecksum(packageId, file) == -1) {
+      logger.info("No code changes detected, skipping package upload");
+    } else {
+      logger.info("Package uploaded");
+    }
+    return file.length();
   }
 }
